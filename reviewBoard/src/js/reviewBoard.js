4eedbdc2133e1c020f70d838862b46acc79e349d
varunsurpurkar <varunsurpurkar@gmail.com>
2016-03-30 23:41:30 -0400

    Initial version of pluggable review board

---
+++ reviewBoard/src/js/reviewBoard.js
@@ -0,0 +1,11 @@
+var app=angular.module('reviewBoard',[]);
+
+app.controller('displayReview',['$scope'] function($scope){
+
+
+});
+
+app.controller('enterReview',['$scope','$http'] function($scope,$http){
+
+	
+});
