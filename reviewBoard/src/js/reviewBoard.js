743293967e9d9be08d6c8b66af2061d8183befe8
varunsurpurkar <varunsurpurkar@gmail.com>
2016-04-14 21:12:58 -0400

    Added server code to serve static pages

--- reviewBoard/src/js/reviewBoard.js
+++ reviewBoard/src/js/reviewBoard.js
@@ -1,11 +1,21 @@
 var app=angular.module('reviewBoard',[]);
 
-app.controller('displayReview',['$scope'] function($scope){
+app.controller('displayReview',['$scope','$http',function($scope,$http){
+ console.log("inside the controller");
+  $http({
+  method: 'GET',
+  url: 'reviews.json'
+}).then(function successCallback(response) {
+       console.log(response.data);
+       $scope.reviews=response.data;
+  }, function errorCallback(errResponse) {
+    console.log(errResponse);
+  });
+
+}]);
 
 
-});
-
-app.controller('enterReview',['$scope','$http'] function($scope,$http){
+app.controller('enterReview',['$scope','$http',function($scope,$http){
 
 	
-});
+}]);
