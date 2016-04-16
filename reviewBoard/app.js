b5e80cc8eac7368b29b91cdbe94a970621c6a0b1
varunsurpurkar <varunsurpurkar@users.noreply.github.com>
2016-04-16 13:40:12 -0400

    Delete app.js
    renamed to server.js

--- reviewBoard/app.js
+++
@@ -1,8 +0,0 @@
-var express=require('express');
-var app=express();
-app.listen(3000);
-
-app.use(express.static('bower_components'));
-app.use(express.static('src/js'));
-app.use(express.static('src/view'));
-app.use(express.static('static'));
